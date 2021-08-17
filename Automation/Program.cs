2262dcb17c6f9b2b66c4362e03f46ff67aed98fd
Nathan Crum <nathan.crum@outlook.com>
2021-08-17 09:39:55 -0400

    starts base console app

--- Automation/Program.cs
+++ Automation/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Automation
 {
@@ -10,7 +6,7 @@ namespace Automation
     {
         static void Main(string[] args)
         {
-            Methods.SearchWeb();
+            Console.WriteLine("Hello World!");
         }
     }
 }
